602942d03b7b06d59260f99d1cbe0ded5db38593
Viktor M <vityok@gmail.com>
2016-10-24 13:12:15 +0300

    fix: renamed image_tag to image_path'

--- Main/admin.py
+++ Main/admin.py
@@ -4,8 +4,8 @@ from Main.models import Post, Like
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'url', 'image_tag', 'added_at')
-    list_display_links = ('url', 'id', 'image_tag')
+    list_display = ('id', 'title', 'author', 'url', 'image_path', 'added_at')
+    list_display_links = ('url', 'id', 'image_path')
     list_editable = ('title', 'author')
 
 admin.site.register(Post, PostAdmin)
