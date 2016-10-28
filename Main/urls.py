ccfaffb14eccdaf542ba4ded0640b07c9f0e69f8
Viktor M <vityok@gmail.com>
2016-10-28 12:15:56 +0300

    add: POST form add: Like is now AJAX POST call

--- Main/urls.py
+++ Main/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 
 
-from Main.views import main, like_post
+from Main.views import main, like_post, add_post
 
 
 urlpatterns = [
-    url(r'^/?$', main),
-    url(r'like/(\d+)/?$', like_post)
+    url(r'^$', main, name='main'),
+    url(r'^like/(\d+)/?$', like_post, name='like_post'),
+    url(r'^add/?$', add_post, name='add_post')
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,7 @@
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.models import User
@@ -13,11 +16,40 @@ def main(request):
 
     return render(request, 'main.html', {'posts': all_posts, 'liked_posts_ids': liked_posts_ids,})
 
-
+@login_required
 def like_post(request, post_id):
     post = get_object_or_404(Post, id=int(post_id))
     like, created = Like.objects.get_or_create(post=post, user=request.user)
     if not created:
         like.delete()
+        return HttpResponse('-1')
+    return HttpResponse('+1')
 
-    return HttpResponseRedirect('/')
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        print(request.POST)
+        title = request.POST.get('title', '')
+        url_ = request.POST.get('url', '')
+        image_file = request.POST.get('image_file', '')
+        comments = request.POST.get('comments','')
+        if title and (url_ or image_file):
+            if Post.objects.filter(url=url_, title=title).count():
+                context = dict(
+                message = 'Bayan!<br/> [:|||||||||||||||||:]',
+                message_type = 'danger',
+                )
+                return render(request, 'add_post.html', context)
+
+            Post.objects.create(title=title, url=url_, comment=comments, author=request.user)
+        else:
+            context = dict(
+                message='Empty TITLE or URL',
+                message_type='danger',
+            )
+            return render(request, 'add_post.html', context)
+        return HttpResponseRedirect(reverse('main'))
+
+    else:
+        return render(request, 'add_post.html', {})
